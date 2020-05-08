62636a6ae6fda8a5dc1b735b1d54dcb9a7271cea
FishInWater-1999 <814484626@qq.com>
2020-05-09 00:22:19 +0800

    Update BookTabs.dart

--- lib/pages/tabs/BookTabs.dart
+++ lib/pages/tabs/BookTabs.dart
@@ -5,17 +5,15 @@ import 'package:flutter/material.dart';
 import 'package:xbooks/utils/UrlHelper.dart';
 
 class BookTabs extends StatefulWidget {
-
   String _category;
 
-  BookTabs(this._category,{Key key}) : super(key: key);
+  BookTabs(this._category, {Key key}) : super(key: key);
 
   @override
   _BookTabsState createState() => _BookTabsState(this._category);
 }
 
 class _BookTabsState extends State<BookTabs> {
-
   String _category;
 
   var _booksList = [];
@@ -24,29 +22,62 @@ class _BookTabsState extends State<BookTabs> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     _getBooks();
   }
 
   _getBooks() async {
-    Response response = await Dio().get(GET_BOOKS_BY_NAME + this._category);
-    setState((){
-      this._booksList = json.decode(response.data);
+    var apiUrl = GET_BOOKS_BY_NAME + this._category;
+    Response response = await Dio().get(apiUrl);
+    setState(() {
+      this._booksList = response.data;
     });
+  }
+
+  Widget _getBook(context, index) {
+    return Container(
+      alignment: Alignment.center,
+      width: double.infinity,
+      height: double.infinity,
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Image.network(
+                _booksList[index]["cover_url"] != null
+                    ? this._booksList[index]["cover_url"]
+                    : "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1588961564404&di=b30ff50e01cf8572e4e85059a7e8777a&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F4efb1c90277789d79e668fdf08c9f336623651231873e-L7DOWH_fw658",
+                fit: BoxFit.contain),
+            SizedBox(height: 12),
+            Text(
+              _booksList[index]['title'],
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20),
+            )
+          ],
+        ),
+      ),
+      decoration: BoxDecoration(
+          border:
+              Border.all(color: Color.fromRGBO(233, 233, 233, 0.9), width: 1)),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-       child: Scaffold(
-         body: this._booksList.length > 0 ? ListView(
-           children: this._booksList.map((value){
-           return ListTile(
-             title: Text(value["666"])
-           );
-         }).toList(),
-       ):Text("loading...")),
+      child: Scaffold(
+          body: this._booksList.length > 0
+              ? GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisSpacing: 10.0,
+                      mainAxisSpacing: 10.0,
+                      crossAxisCount: 2),
+                  itemCount: this._booksList.length,
+                  itemBuilder: this._getBook,
+                )
+              : Center(
+                  child: Image.network(
+                      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1588960394935&di=516d609eb475b579a79319f7c40e6b28&imgtype=0&src=http%3A%2F%2Fimg2.imgtn.bdimg.com%2Fit%2Fu%3D2022540562%2C3971311545%26fm%3D214%26gp%3D0.jpg"))),
     );
   }
 }
