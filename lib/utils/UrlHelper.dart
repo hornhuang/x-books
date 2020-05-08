8e393d3048b42e0a77a5ed5ca5d163440e5fb039
FishInWater-1999 <814484626@qq.com>
2020-05-08 18:46:16 +0800

    Create UrlHelper.dart

---
+++ lib/utils/UrlHelper.dart
@@ -0,0 +1,5 @@
+var ALL_BOOKS    = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${MY_KEY}";
+
+var AUTHOR_BOOKS = "https://api.nytimes.com/svc/books/v3/reviews.json?author=Stephen+King&api-key=${MY_KEY}";
+
+var MY_KEY       = "nHvk2jaUl2Zgv6gWAHoCkrN5A1bRWuU2";
