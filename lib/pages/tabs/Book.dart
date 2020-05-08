14800563ad4c64bbd0a15531d64a41544d89bec7
FishInWater-1999 <814484626@qq.com>
2020-05-08 18:40:05 +0800

    Update Book.dart

--- lib/pages/tabs/Book.dart
+++ lib/pages/tabs/Book.dart
@@ -11,9 +11,38 @@ class _BookPageState extends State<BookPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-       child: Center(
-          child: Text("BookPage"),
-        ),
+       child: ListView(
+         padding: EdgeInsets.all(10),
+         children: <Widget>[
+          ListTile(
+            leading: Container(
+              width: 75,
+              height: 125,
+              child: Image.network("https://www.itying.com/images/flutter/1.png", fit: BoxFit.cover,),
+            ),
+            title: Text('华北黄淮高温雨今起强势登场'),
+            subtitle: Text('中国天气网讯 21日开始，华北黄淮高温雨今起强势登场'),
+          ),
+          Divider(),
+          ListTile(
+            leading: Container(
+              width: 75,
+              height: 125,
+            child: Image.network("https://www.itying.com/images/flutter/1.png", fit: BoxFit.cover),
+            ),title: Text('华北黄淮高温雨今起强势登场'),
+            subtitle: Text('中国天气网讯 21日开始，华北黄淮高温雨今起强势登场'),
+          ),
+          Divider(),
+          ListTile(
+            leading: Container(
+              width: 75,
+              height: 125,
+            child: Image.network("https://www.itying.com/images/flutter/1.png", fit: BoxFit.cover),
+            ),title: Text('华北黄淮高温雨今起强势登场'),
+            subtitle: Text('中国天气网讯 21日开始，华北黄淮高温雨今起强势登场'),
+          ),
+         ]
+       )
     );
   }
 }
