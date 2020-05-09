cce64c788357aadb0c00504ca3b4cfcd864c7eb6
FishInWater-1999 <814484626@qq.com>
2020-05-09 09:32:15 +0800

    Update Routes.dart

--- lib/routes/Routes.dart
+++ lib/routes/Routes.dart
@@ -9,7 +9,7 @@ import 'package:xbooks/pages/tabs/BookDetail.dart';
 //配置路由
 final routes = {
   '/': (context) => Tabs(),
-  '/bookdetail': (context, arguments) => WebViewExample(arguments)
+  '/bookdetail': (context, {arguments}) => WebViewExample(arguments: arguments)
 };
 
 //固定写法
