4fb8dce05ffd8a023eb40833b1cc2a64a79dd39e
FishInWater-1999 <814484626@qq.com>
2020-05-09 16:43:19 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,17 @@ import 'package:xbooks/routes/Routes.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: true,
       home: Tabs(),
       onGenerateRoute: onGenerateRoute,
+      theme: ThemeData(
+          dividerTheme: DividerThemeData(
+              color:  Colors.black38,
+              space: 0.6,
+              thickness: 0.6)),
     );
   }
-
 }
