bf4557f5fac80c5675d325e0f13d900fb6efbab1
FishInWater-1999 <814484626@qq.com>
2020-05-09 17:27:28 +0800

    Update Home.dart

--- lib/pages/tabs/Home.dart
+++ lib/pages/tabs/Home.dart
@@ -10,8 +10,9 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Center(
+    return Material(
+        //  tth:,
+       child: Container(
           child: Text("HomePage"),
         ),
     );
