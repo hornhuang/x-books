00acdce259cddbb066ff85913b44535a4d5839be
FishInWater-1999 <814484626@qq.com>
2020-05-09 23:17:37 +0800

    Update Tab.dart

--- lib/pages/Tab.dart
+++ lib/pages/Tab.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:xbooks/pages/tabs/Book.dart';
 import 'package:xbooks/pages/tabs/Find.dart';
 import 'package:xbooks/pages/tabs/Home.dart';
+import 'package:xbooks/utils/PreferencesHelper.dart';
 
 class Tabs extends StatefulWidget {
   Tabs
@@ -20,13 +21,34 @@ class _TabsState extends State<Tabs> {
     HomePage()
   ];
 
+  int themeIndex;
+
+  var themeIcons = [
+    Icons.wb_sunny,
+    Icons.wb_cloudy
+  ];
+
+  @override
+  void initState() { 
+    super.initState();
+    themeIndex = MyTheme.isDarkTheme()?1:0;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
        child: Scaffold(
 
         appBar: AppBar(
-          title: Text("X-Books")
+          title: Text("X-Books"),
+          actions: <Widget>[
+            IconButton(icon: Icon(themeIcons[themeIndex]), onPressed: () {
+              MyTheme.updateThme(!MyTheme.isDarkTheme());
+              setState(() {
+                this.themeIndex = themeIndex = MyTheme.isDarkTheme()?1:0;
+              });
+            })
+          ],
         ),
         body: _pageLists[this._concurrentindex],
 
